37171cc94b9a26d23896e3153f76f10855fb3513
wuhs <wuhs@si-tech.com.cn>
2024-10-28 23:01:16 +0800

    first commit

---
+++ src/main/java/com/coderwhs/init/mapper/PostThumbMapper.java
@@ -0,0 +1,15 @@
+package com.coderwhs.init.mapper;
+
+import com.coderwhs.init.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/coderwhs/init/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.coderwhs.init.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.coderwhs.init.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
