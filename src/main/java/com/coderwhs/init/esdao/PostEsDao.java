5f8b485261c2cf5ba10dc1403e023014ecff18ef
coderwhs <coderwhs@gmail.com>
2024-10-28 23:01:16 +0800

    first commit

---
+++ src/main/java/com/coderwhs/init/esdao/PostEsDao.java
@@ -0,0 +1,13 @@
+package com.coderwhs.init.esdao;
+
+import com.coderwhs.init.model.dto.post.PostEsDTO;
+import java.util.List;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 帖子 ES 操作
+ */
+public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
+
+    List<PostEsDTO> findByUserId(Long userId);
+}
